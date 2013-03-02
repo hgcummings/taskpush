56b1f15d220535b527c216067e8e88ac23d44a26
Harry Cummings <harry@xlr3.com>
2013-03-02 14:36:25 +0000

    Fix indendation

--- web.js
+++ web.js
@@ -6,46 +6,46 @@ var uuid = require('node-uuid');
 app.use(express.bodyParser());
 
 var pushTask = function(req, res){
-  var id = req.param("id", uuid.v1());
-  console.info({ type: "request", identifier: id, query: req.query, body: req.body});
+    var id = req.param("id", uuid.v1());
+    console.info({ type: "request", identifier: id, query: req.query, body: req.body});
 
-  var taskReq = require('request'),
-   username = "username",
-   password = "password",
-   auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64'),
-   url = "http://checkvist.com/checklists/checklist_id/tasks.json";
+    var taskReq = require('request'),
+        username = "username",
+        password = "password",
+        auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64'),
+        url = "http://checkvist.com/checklists/checklist_id/tasks.json";
 
-  req.param("text").split("\n").forEach(function(taskContent) {
-    taskReq.post(
-      {
-    	  headers : { 'Authorization': auth },
-        url: url,
-        body: "task[content]=" + taskContent + " ^ASAP"
-      },
-      function(error, response, body) {  
-        if (error) {
-          // This indicates a transport error rather than an error response from checkvist
-          console.error({ type: "response", identifier: id, error: error});
-          res.send("", 500);
-        } else {
-          console.info({ type: "response", identifier: id, body: body});
-          // Always send an empty response, since we don't want to pay for a return message
-          // Pass checkvist response code back to the caller, so they can retry if necessary
-          res.send("", response.statusCode);
-        }
-      }
-    );
-  });
+    req.param("text").split("\n").forEach(function(taskContent) {
+        taskReq.post(
+            {
+                headers : { 'Authorization': auth },
+                url: url,
+                body: "task[content]=" + taskContent + " ^ASAP"
+            },
+            function(error, response, body) {
+                if (error) {
+                    // This indicates a transport error rather than an error response from checkvist
+                    console.error({ type: "response", identifier: id, error: error});
+                    res.send("", 500);
+                } else {
+                    console.info({ type: "response", identifier: id, body: body});
+                    // Always send an empty response, since we don't want to pay for a return message
+                    // Pass checkvist response code back to the caller, so they can retry if necessary
+                    res.send("", response.statusCode);
+                }
+            }
+        );
+    });
 };
 
 // Nexmo will only use our URL if we respond to a HEAD request with an HTTP 200
-app.head('/tasks/', function(req, res) {
-  res.send("", 200);
+app.all('/tasks/', function(req, res) {
+    res.send("", 200);
 });
 app.get('/tasks/', pushTask);
 app.post('/tasks/', pushTask);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
