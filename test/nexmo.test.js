8c63f7f8e9888e89201294f87db04f7826eb5584
Harry Cummings <harry@xlr3.com>
2013-03-02 18:42:46 +0000

    Add tests

---
+++ test/nexmo.test.js
@@ -0,0 +1,10 @@
+require('assert');
+require('../lib/nexmo.js');
+
+describe('nexmo', function() {
+   describe('#configure()', function() {
+        it ('should set up an HTTP OK response for a HEAD request', function() {
+
+        });
+    });
+});
