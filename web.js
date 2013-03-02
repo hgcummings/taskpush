265cf970e073d89dfa99a0f0208fbfe0ecaefa84
Harry Cummings <harry@xlr3.com>
2013-03-02 14:33:35 +0000

    Initial task push functionality

--- web.js
+++ web.js
@@ -2,8 +2,31 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(express.bodyParser());
+
+app.post('/tasks/', function(req, res){
+  console.log(req.body.message);
+
+  var taskReq = require('request'),
+   username = "username",
+   password = "password",
+   auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64'),
+   url = "http://checkvist.com/checklists/checklist_id/tasks.json";
+
+  taskReq.post({
+  	  headers : {
+        'Authorization': auth
+  	  },
+      url: url,
+      body: "task[content]=" + req.body.message
+    }, function(error, response, body) {
+      if (error) {
+        console.log(error);
+        res.send(500);
+      } else {
+      	res.send(204);
+      }
+  });
 });
 
 var port = process.env.PORT || 5000;
