9f0425f211e23a58fc77cda333880c61050c4e08
Harry Cummings <harry@xlr3.com>
2013-03-09 22:17:36 +0000

    Enable trust proxy (for Heroku)

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ app.use(express.bodyParser());
 require('./lib/nexmo/controller.js')(app, '/nexmo/');
 
 var port = process.env.PORT || 5000;
+app.enable('trust proxy');
 app.listen(port, function() {
     console.log('Listening on ' + port);
 });
