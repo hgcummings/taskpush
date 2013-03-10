617d17ede0a0f805bcfead11a4db0581a3c66684
Harry Cummings <harry@xlr3.com>
2013-03-10 16:29:22 +0000

    Logging

--- server/forceHttps.js
+++ server/forceHttps.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function forceHttps(request, response, next) {
+    console.info(request.host);
+    console.info(request.protocol);
     if (request.host === 'localhost' || request.secure) {
         next();
     } else {
